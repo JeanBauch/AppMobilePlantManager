2b75f4be634f80f38a7703e9d10d3bda98d51a86
computeiros <drivecomputeiros@gmail.com>
2021-05-03 18:08:13 -0300

    DevPlantManager

---
+++ src/pages/MyPlants.tsx
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, Image, FlatList, Alert } from 'react-native';
+
+import { Header } from '../components/Header'
+
+import colors from '../styles/colors';
+import waterdrop from '../assets/waterdrop.png';
+import { loadPlant, PlantProps, removePlant } from '../libs/storage';
+import { formatDistance } from 'date-fns';
+import { pt } from 'date-fns/locale';
+import fonts from '../styles/fonts';
+import { PlantCardSecundary } from '../components/PlantCardSecundary';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Load } from '../components/Load';
+
+export function MyPlants() {
+  const [myPlants, setMyPlants] = useState<PlantProps[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [nextWaterd, setNextWatered] = useState<string>();
+
+  function handleRemove(plant: PlantProps) {
+    Alert.alert('Remover', `Deseja remover a ${plant.name}`, [
+      {
+        text: 'Nao 🙏',
+        style: 'cancel'
+      },
+      {
+        text: 'Sim 😥',
+        onPress: async () => {
+          try {
+            await removePlant(plant.id);
+
+            setMyPlants((oldData) => 
+              oldData.filter((item) => item.id != plant.id)
+            );
+
+          } catch (error) {
+            Alert.alert('Nao foi possivel remover! 😥');
+          }
+        }
+      }
+    ])
+  }
+
+  useEffect(() => {
+    async function loadStorageDate() {
+      const plantsStoraged = await loadPlant();
+
+      const nextTime = formatDistance(
+        new Date(plantsStoraged[0].dateTimeNotification).getTime(),
+        new Date().getTime(),
+        { locale: pt }
+      );
+
+      setNextWatered(
+        `Nao esqueca de regar a ${plantsStoraged[0].name} a ${nextTime} horas.`
+      )
+      setMyPlants(plantsStoraged);
+      setLoading(false);
+    }
+
+    loadStorageDate();
+  },[]);
+
+
+  if(loading)
+    return <Load />
+
+  return (
+    <View style={styles.container}>
+      <Header />
+
+      <View style={styles.spotlight}>
+        <Image 
+          source={waterdrop}
+          style={styles.spotlightImage}
+        />
+
+        <Text style={styles.spotlightText}>
+          {nextWaterd}
+        </Text>
+      </View>
+
+      <View style={styles.plants}>
+        <Text style={styles.plantsTitle}>
+          Proximas regadas
+        </Text>
+        
+        <FlatList
+          data={myPlants}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={( {item} ) => (
+            <PlantCardSecundary 
+              data={item}
+              handleRemove={() => {handleRemove(item)}}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 30,
+    paddingTop: 50,
+    backgroundColor: colors.background
+  },
+  spotlight: {
+    backgroundColor: colors.blue_light,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    height: 110,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  spotlightImage: {
+    width: 60,
+    height: 60,
+  },
+  spotlightText: {
+    flex: 1,
+    color: colors.blue,
+    paddingHorizontal: 20,
+    textAlign: 'justify'
+  },
+  plants: {
+    flex: 1,
+    width: '100%'
+  },
+  plantsTitle: {
+    fontSize: 24,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    marginVertical: 20,
+  },
+})
